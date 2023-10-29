6aa6d841933a26811329690887821ec5a27e2ff2
Spike <sledsib@ya.ru>
2023-10-29 23:29:46 +0300

    feat(model): Added model for coordinates

---
+++ src/model/coord-model.ts
@@ -0,0 +1,74 @@
+import { ModelInterface } from '../interface/model.interface';
+
+type Coords = [number, number];
+
+enum Coord {X, Y}
+
+export default class CoordModel implements ModelInterface<Coords> {
+  private count = 0;
+
+  private coords: Coords[];
+
+  constructor(count: number) {
+    this.coords = [];
+    this.count = count;
+
+    this.setCollection();
+  }
+
+  setCollection() {
+    for (let i = 0; i < this.count; i++) {
+      for (let j = 0; j < this.count; j++) {
+        this.coords.push([j, i]);
+      }
+    }
+  }
+
+  getCollection() {
+    return [...this.coords];
+  }
+
+  groupCollectionByColumn() {
+    return this.coords.reduce((groupedCoords: Coords[][], coords: Coords) => {
+      const x = coords[Coord.X];
+
+      groupedCoords[x] = groupedCoords[x] || [];
+      groupedCoords[x].push(coords);
+
+      return groupedCoords;
+    }, []);
+  }
+
+  groupCollectionByColumnReverse() {
+    return this.coords.reduce((groupedCoords: Coords[][], coords: Coords) => {
+      const x = coords[Coord.X];
+
+      groupedCoords[x] = groupedCoords[x] || [];
+      groupedCoords[x].unshift(coords);
+
+      return groupedCoords;
+    }, []);
+  }
+
+  groupCollectionByRow() {
+    return this.coords.reduce((groupedCoords: Coords[][], coords: Coords) => {
+      const y = coords[Coord.Y];
+
+      groupedCoords[y] = groupedCoords[y] || [];
+      groupedCoords[y].push(coords);
+
+      return groupedCoords;
+    }, []);
+  }
+
+  groupCollectionByRowReverse() {
+    return this.coords.reduce((groupedCoords: Coords[][], coords: Coords) => {
+      const y = coords[Coord.Y];
+
+      groupedCoords[y] = groupedCoords[y] || [];
+      groupedCoords[y].unshift(coords);
+
+      return groupedCoords;
+    }, []);
+  }
+}
